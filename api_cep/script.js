8f7c23065fee148efa6358a0f0383c7170d0f30c
Vinicius <vini.barriquelo@gmail.com>
2021-09-24 13:33:30 -0300

    Refaroraçao do projeto

---
+++ api_cep/script.js
@@ -0,0 +1,32 @@
+function consultaCep() {
+  const cep = document.querySelector("#cep").value;
+
+  if (cep == "" || cep.length != 8) {
+    alert("Digite um CEP válido");
+    return;
+  }
+
+  let requestURL = `https://viacep.com.br/ws/${cep}/json/`;
+
+  var request = new XMLHttpRequest();
+
+  request.open("GET", requestURL);
+
+  request.responseType = "json";
+  request.send();
+
+  request.onload = function () {
+    let cepConsultado = request.response;
+
+    const bairro = cepConsultado.bairro;
+    const rua = cepConsultado.logradouro;
+    const cidade = cepConsultado.localidade;
+
+    document.querySelector("#rua").value = `${rua}`;
+    document.querySelector("#bairro").value = `${bairro}`;
+    document.querySelector("#cidade").value = `${cidade}`;
+  };
+}
+
+const btn = document.querySelector("#btn-consulta");
+btn.addEventListener("click", consultaCep);
